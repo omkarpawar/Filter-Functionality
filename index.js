4dd99aa236e7d84c402d4a4c9ec5cf743c57bc0c
omkarpawar <omkar111pawar@gmail.com>
2024-01-19 21:20:40 +0530

    first commit

--- index.js
+++ index.js
@@ -1,41 +1,16 @@
-const form=document.querySelector('form');
-const fruits=document.querySelector('.fruits');
+const filter =document.getElementById('filter');
 
-const editBtn=document.createElement('button');
-const editBtnTxt=document.createTextNode('Edit');
-editBtn.appendChild(editBtnTxt);
-editBtn.className='edit-btn'
+filter.addEventListener('keyup',function(event){
+    const textEnterd=event.target.value.toLowerCase();
+    const fruitItems=document.getElementsByClassName('fruit');
 
-const lis=document.querySelectorAll('.fruit');
-
-lis.forEach(function(lis){
-    const editBtn=document.createElement('button');
-    const editBtnTxt=document.createTextNode('Edit');
-    editBtn.appendChild(editBtnTxt);
-    editBtn.className='edit-btn'
-    lis.appendChild(editBtn)
-})
-
-
-
-
-
-
-form.addEventListener('submit',function(event){
-    event.preventDefault();
-    const fruitToAdd=document.getElementById('fruit-to-add');
-    
-    const newLi=document.createElement('li');
-    newLi.innerHTML=fruitToAdd.value+'<button class="delete-btn">x</button>'+'<button class="edit-btn">Edit</button>'
-    fruits.appendChild(newLi);
-})
-
-
-    
-    
-fruits.addEventListener('click',function(event){
-    if (event.target.classList.contains('delete-btn')){
-       const fruitToDelete=event.target.parentElement;
-       fruits.removeChild(fruitToDelete); 
+    for(let i=0;i<fruitItems.length;i++){
+        
+        const currentFruit=fruitItems[i].firstChild.textContent.toLocaleLowerCase()
+        if(currentFruit.indexOf(textEnterd)=== -1){
+            fruitItems[i].style.display="none";
+        }else{
+            fruitItems[i].style.display="flex";
+        }
     }
 })
